b96dfd18b9fad2c16d1dc5b82ee3d8863edfbf61
Yuexin Chen <u7194943@anu.edu.au>
2021-04-01 21:19:34 +0800

    Success in pics animation.

--- Pokemon_Battle/src/GUI.java
+++ Pokemon_Battle/src/GUI.java
@@ -116,9 +116,9 @@ public class GUI extends Application {
         i.setLayoutX(-100);
         i.setLayoutY(-100);
 
-        String IMG = getClass().getResource("Pics/pic0.jpg").toString();
+        Image img = new Image("Pics/pic0.jpg");
         Rectangle rect = new Rectangle(300, 300, 200, 200);
-        rect.setFill(new ImagePattern(new Image(IMG)));
+        rect.setFill(new ImagePattern(img));
         Path path = new Path();
         path.getElements().add(new MoveTo(300, 300));
         path.getElements().add(new LineTo(700, 300));
