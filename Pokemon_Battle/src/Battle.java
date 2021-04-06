3a3399a04467f18f9ffd528f87a2529dd57d934c
Yuexin Chen <u7194943@anu.edu.au>
2021-04-06 10:23:26 +0800

    Update Battle.java

--- Pokemon_Battle/src/Battle.java
+++ Pokemon_Battle/src/Battle.java
@@ -1,9 +1,12 @@
 
+import javafx.beans.property.StringProperty;
+
 public class Battle {
     private int turn = 0;
 
     private Enemy tar;
     private Pokemon user;
+
 
     public int getTurn() {
         return turn;
@@ -20,11 +23,32 @@ public class Battle {
         return this.tar.getHP() == 0 || this.user.getHP() == 0;
     }
 
-    public void user_turn (int action){
-
+    // return true for user turn, false for enemy turn
+    public boolean user_turn (){
+        return this.turn % 2 == 0;
     }
 
-    public void enemy_turn () {
+    // execute user action, 0 for attack, 1 for spell-1, 2 for spell-2, ...
+    public void user_action (int action, StringProperty info){
+        System.out.println("Execute action: " + action);
+        this.turn ++;
+        info.setValue("Waiting for enemy response.");
+        System.out.println(info.get());
+        this.enemy_action(info);
+    }
+
+    public void enemy_action (StringProperty info) {
+        System.out.println("Now for enemy turn.");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        this.turn ++;
+        info.setValue("Now is your turn.");
+        System.out.println(info.get());
+        System.out.println("Enemy turn finished.");
 
     }
 
