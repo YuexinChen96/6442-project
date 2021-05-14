6479e2922802d06197488bd1d34176118c9f4d08
natalierqx <natalierqx@gmail.com>
2021-05-14 23:08:25 +0800

    add skill list to enemy

--- Pokemon_Battle/src/Enemy.java
+++ Pokemon_Battle/src/Enemy.java
@@ -92,13 +92,18 @@ public class Enemy implements Serializable {
         this.id = id;
     }
 
-    public Enemy(int id, String name, int attack, int defence, int HP, int MP){
+    public int[] getSkillList(){return skill_list;}
+
+    public void setSkillList(int[] skill_list){this.skill_list=skill_list;}
+
+    public Enemy(int id, String name, int attack, int defence, int HP, int MP,int[]skill_list){
         this.id = id;
         this.name = name;
         this.attack = attack;
         this.defence = defence;
         this.HP = HP;
         this.MP = MP;
+        this.skill_list=skill_list;
     }
 
 }
