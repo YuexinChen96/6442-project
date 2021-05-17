22829c069b8a53b7d58b26370660ab3f7536d129
Yifei Tao <u7078518@anu.edu.au>
2021-05-17 11:04:08 +0800

    add Tests

---
+++ Pokemon_Battle/tests/pokemonbattle/tests/testPokemon.java
@@ -0,0 +1,22 @@
+package pokemonbattle.tests;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import static org.junit.Assert.assertTrue;
+
+public class testPokemon {
+
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(1000);
+    private void doTest() {
+        assertTrue("1",true);
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+}
